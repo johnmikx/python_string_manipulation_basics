1c49e039a1403d97a90095f45a0ed69fd6e89f5b
John Mike Asuncion <johnmikeasuncion17@gmail.com>
2025-03-19 10:44:40 +0800

    initial commit

---
+++ count_characters.py
@@ -0,0 +1,10 @@
+# Prog08: Create a program that ask the user to input their fullname. 
+# Print the number of characters in the input.
+
+# Example:
+# Input: Juan Dela Cruz
+# Output: 14
+
+# ------------------------------------------------------------------
+
+# Phase 1: Planning
---
+++ snake_case_conversion.py
@@ -0,0 +1,10 @@
+# Prog10: Create a program that ask the user to input their fullname 
+# in incorrect casing. Print the input in snake case.
+
+# Example:
+# Input: jUAn DEla CrUZ
+# Output: juan_dela_cruz
+
+# ------------------------------------------------------------------
+
+# Phase 1: Planning
